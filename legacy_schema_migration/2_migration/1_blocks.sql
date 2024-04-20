68254430abd8f1817869b5562dbf2b9bd166c9a5
supertypo <suprtypo@pm.me>
2024-04-20 15:45:18 +0200

    Do all blocks column altering in one statement

--- legacy_schema_migration/2_migration/1_blocks.sql
+++ legacy_schema_migration/2_migration/1_blocks.sql
@@ -24,23 +24,24 @@ WHERE is_chain_block = true;
 ALTER TABLE chain_blocks ADD PRIMARY KEY (block_hash);
 
 -- Change datatypes on blocks
-ALTER TABLE blocks ALTER COLUMN hash TYPE BYTEA USING DECODE(hash, 'hex');
-ALTER TABLE blocks ALTER COLUMN accepted_id_merkle_root TYPE BYTEA USING DECODE(accepted_id_merkle_root, 'hex');
---difficulty stays the same
-ALTER TABLE blocks DROP COLUMN is_chain_block;
-ALTER TABLE blocks ALTER COLUMN merge_set_blues_hashes TYPE BYTEA[] USING decode_varchar_array(merge_set_blues_hashes);
-ALTER TABLE blocks ALTER COLUMN merge_set_reds_hashes TYPE BYTEA[] USING decode_varchar_array(merge_set_reds_hashes);
-ALTER TABLE blocks ALTER COLUMN selected_parent_hash TYPE BYTEA USING DECODE(selected_parent_hash, 'hex');
-ALTER TABLE blocks ALTER COLUMN bits TYPE BIGINT;
---blue_score stays the same
-ALTER TABLE blocks ALTER COLUMN blue_work TYPE BYTEA USING CONVERT_TO(blue_work, 'UTF8'); -- Store large number as bytea to save space
-ALTER TABLE blocks ALTER COLUMN hash_merkle_root TYPE BYTEA USING DECODE(hash_merkle_root, 'hex');
-ALTER TABLE blocks ALTER COLUMN nonce TYPE NUMERIC(32,0) USING CONVERT_TO(nonce, 'UTF8'); -- Store large number as bytea to save space
-ALTER TABLE blocks ALTER COLUMN parents TYPE BYTEA[] USING decode_varchar_array(parents);
-ALTER TABLE blocks ALTER COLUMN pruning_point TYPE BYTEA USING DECODE(pruning_point, 'hex');
-ALTER TABLE blocks ALTER COLUMN timestamp TYPE INTEGER USING EXTRACT(EPOCH FROM timestamp)::INTEGER;
-ALTER TABLE blocks ALTER COLUMN utxo_commitment TYPE BYTEA USING DECODE(utxo_commitment, 'hex');
-ALTER TABLE blocks ALTER COLUMN version TYPE SMALLINT;
+ALTER TABLE blocks
+    ALTER COLUMN hash TYPE BYTEA USING DECODE(hash, 'hex'),
+    ALTER COLUMN accepted_id_merkle_root TYPE BYTEA USING DECODE(accepted_id_merkle_root, 'hex'),
+    --difficulty stays the same
+    DROP COLUMN is_chain_block,
+    ALTER COLUMN merge_set_blues_hashes TYPE BYTEA[] USING decode_varchar_array(merge_set_blues_hashes),
+    ALTER COLUMN merge_set_reds_hashes TYPE BYTEA[] USING decode_varchar_array(merge_set_reds_hashes),
+    ALTER COLUMN selected_parent_hash TYPE BYTEA USING DECODE(selected_parent_hash, 'hex'),
+    ALTER COLUMN bits TYPE BIGINT,
+    --blue_score stays the same
+    ALTER COLUMN blue_work TYPE BYTEA USING CONVERT_TO(blue_work, 'UTF8'), -- Store large number as bytea to save space
+    ALTER COLUMN hash_merkle_root TYPE BYTEA USING DECODE(hash_merkle_root, 'hex'),
+    ALTER COLUMN nonce TYPE NUMERIC(32,0) USING CONVERT_TO(nonce, 'UTF8'), -- Store large number as bytea to save space
+    ALTER COLUMN parents TYPE BYTEA[] USING decode_varchar_array(parents),
+    ALTER COLUMN pruning_point TYPE BYTEA USING DECODE(pruning_point, 'hex'),
+    ALTER COLUMN timestamp TYPE INTEGER USING EXTRACT(EPOCH FROM timestamp)::INTEGER,
+    ALTER COLUMN utxo_commitment TYPE BYTEA USING DECODE(utxo_commitment, 'hex'),
+    ALTER COLUMN version TYPE SMALLINT;
 
 -- Rename index
 ALTER INDEX idx_blue_score RENAME TO idx_blocks_blue_score;
