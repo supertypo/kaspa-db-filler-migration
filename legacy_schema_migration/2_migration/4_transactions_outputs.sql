b61164576ba2ba6b8d128ec89a98247466638dcf
supertypo <suprtypo@pm.me>
2024-04-29 16:17:40 +0200

    4_transactions_outputs.sql: Removed invalid reference to block_time

--- legacy_schema_migration/2_migration/4_transactions_outputs.sql
+++ legacy_schema_migration/2_migration/4_transactions_outputs.sql
@@ -14,7 +14,7 @@ SELECT create_partition('transactions_outputs', 'transactions_outputs_p', 32);
 -- We need a primary key to handle duplicates:
 ALTER table transactions_outputs ADD PRIMARY KEY (transaction_id, index);
 
-INSERT INTO transactions_outputs (transaction_id, index, amount, script_public_key, script_public_key_address, script_public_key_type, block_time)
+INSERT INTO transactions_outputs (transaction_id, index, amount, script_public_key, script_public_key_address, script_public_key_type)
 SELECT DECODE(o.transaction_id, 'hex'),
        o.index::SMALLINT,
        o.amount,
@@ -29,4 +29,3 @@ DROP TABLE old_transactions_outputs;
 -- Create indexes
 CREATE INDEX ON transactions_outputs (transaction_id);
 CREATE INDEX ON transactions_outputs (script_public_key_address);
-CREATE INDEX ON transactions_outputs (block_time DESC NULLS LAST);
