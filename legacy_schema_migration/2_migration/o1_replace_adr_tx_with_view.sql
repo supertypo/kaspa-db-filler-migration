bd43b1ed4076e4f3508d58a2295a45ea913619e5
supertypo <suprtypo@pm.me>
2024-07-17 11:28:46 +0200

    Small improvement to addresses_transactions view perf

--- legacy_schema_migration/2_migration/o1_replace_adr_tx_with_view.sql
+++ legacy_schema_migration/2_migration/o1_replace_adr_tx_with_view.sql
@@ -12,8 +12,8 @@ ALTER TABLE addresses_transactions RENAME TO addresses_transactions_old;
 
 -- Create view
 CREATE OR REPLACE VIEW addresses_transactions AS
-    SELECT adr_tx.address, t.transaction_id, t.block_time FROM (
-        SELECT DISTINCT tout.script_public_key_address address, UNNEST(ARRAY [tout.transaction_id, tin.transaction_id]) transaction_id
+    SELECT DISTINCT adr_tx.address, t.transaction_id, t.block_time FROM (
+        SELECT tout.script_public_key_address address, UNNEST(ARRAY [tout.transaction_id, tin.transaction_id]) transaction_id
             FROM transactions_outputs tout
             LEFT JOIN transactions_inputs tin ON tout.transaction_id = tin.previous_outpoint_hash AND tout.index = tin.previous_outpoint_index
     ) adr_tx
